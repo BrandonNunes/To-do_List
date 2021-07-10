f5e33116f95acb6ffe46560c443d42b88317fcb4
Brandon Alves <brandonalvesn@gmail.com>
2021-07-10 19:22:30 -0300

    mudança no css e adicionado funcionalidade de mostrar a quantidade de tasks

--- app.js
+++ app.js
@@ -4,24 +4,46 @@ var input =  document.getElementById('input');
     
     
 btn.addEventListener("click", () => {
+
+    if(input.value.length == 0){
+        alert("Por favor adicione algo antes!");
+    }else{
+        list.insertAdjacentHTML(
+            'afterbegin',
+                    `<li>
+                        ${input.value}
+                        <button class="btnr">
+                        &#10006;
+                        </button>
+                    </li>
+                    
+                    `
+        )
+    }
     
-    list.insertAdjacentHTML(
-       `afterbegin`, `
-       <li>
-       ${input.value}
-            <button class="btnr">X</button>
-       </li>`
-    );
+    
    input.value = "";
 
-   let r = document.querySelectorAll(".btnr");
-        r.forEach((del) => {
+   let delet = document.querySelectorAll(".btnr");
+        delet.forEach((del) => {
             del.addEventListener('click',()=>{
                 del.parentElement.remove();
+                
             });
         });
-
+        let totalTasks = document.getElementById("tasksN");
+        totalTasks.innerHTML = `${delet.length}`;
+      
+       
     });
 
+    
+       
+
+
+    
+    
+    
+   
 
 
